651f45348a9ecf2575fef6c0c3028821de4bed5f
Magz-devv <ronaldpaulmaggay2007@gmail.com>
2026-07-30 00:58:25 +0800

    Create practice 01.1.cs

---
+++ practice 01.1.cs
@@ -0,0 +1,24 @@
+class Program
+{
+
+    static void Main(string[] args)
+
+    {
+
+        Console.Write("Enter a number: ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+        if (number % 2 == 0)
+
+        {
+
+            Console.WriteLine("The number " + number + " is even.");
+
+        }
+
+    }
+
+}
