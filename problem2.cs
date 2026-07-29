4dae5ac82d92b56c3c68b8559a052b8e0ab38d2c
Magz-devv <ronaldpaulmaggay2007@gmail.com>
2026-07-30 00:41:22 +0800

    Add Program class to display full name

---
+++ problem2.cs
@@ -0,0 +1,12 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "bobet";
+        string lastName = "baterbonia";
+
+        string fullName = firstName + " " + lastName;
+
+        Console.WriteLine("Full Name: " + fullName);
+    }
+}
